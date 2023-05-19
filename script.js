af01500e9d2c7b9fffedac200867a24e83857fb1
Mara_Balinisteanu <balinisteanumara@yahoo.com>
2023-05-19 20:14:35 +0300

    sda

---
+++ script.js
@@ -0,0 +1,5 @@
+const nume= 'Mihai';
+const initialValues=[17, 5, 'Mara', nume, null, undefined,['Mitzi','Gogo',10,5,2,null]];
+const otherValues=[10,20,nume,'Ana are mere',null, ['Si Mara are mere'],nume,undefined,[5,7]];
+otherValues.push(initialValues);
+console.log(otherValues);
